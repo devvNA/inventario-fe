aea3df299978d7702e1d6bb1e9d540d86c3f0a5f
devvNA <devitazaqi@gmail.com>
2026-04-19 19:14:36 +0700

    chore: configure manual chunk splitting and increase build size limit in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+          query: ["@tanstack/react-query"],
+          ui: ["react-hook-form", "zod", "axios", "react-hot-toast"],
+        },
+      },
+    },
+  },
 });
